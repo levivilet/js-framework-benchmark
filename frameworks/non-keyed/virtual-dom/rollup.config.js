870069e2611cfe7614a02854316a7cfec62d2880
Le Vivilet <levivilet@pm.me>
2026-05-29 21:51:52 +0000

    switch to rollup

---
+++ frameworks/non-keyed/virtual-dom/rollup.config.js
@@ -0,0 +1,18 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import terser from "@rollup/plugin-terser"
+
+const createConfig = (input, file) => ({
+  input,
+  output: {
+    compact: true,
+    file,
+    format: "esm",
+  },
+  plugins: [nodeResolve(), terser()],
+})
+
+/** @type {import("rollup").RollupOptions[]} */
+export default [
+  createConfig("src/main.js", "dist/main.js"),
+  createConfig("src/worker.js", "dist/worker.js"),
+]
