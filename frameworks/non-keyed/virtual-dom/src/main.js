c90c8a11b99ec009a95cb3d9e06d2909b0cafaf0
Le Vivilet <levivilet@pm.me>
2026-05-29 14:53:15 +0000

    fix

--- frameworks/non-keyed/virtual-dom/src/main.js
+++ frameworks/non-keyed/virtual-dom/src/main.js
@@ -14,10 +14,6 @@ const rpc = createRpc(worker, {
     renderInto(app, nodes)
     root = app.firstElementChild
   },
-  renderHtml({ html }) {
-    app.innerHTML = html
-    root = app.firstElementChild
-  },
   patch({ patches }) {
     if (root) {
       applyPatch(root, patches)
